838de58fbbfbb7e446b2a2902bb0c43cf23c4828
Vitaliy <vitaliy.srt1985@gmail.com>
2025-03-09 21:11:07 +0200

    add fastapi app

---
+++ src/utilitiesaccounting_v4/fastapi_app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
+from src.utilitiesaccounting_v4.routers import main_router
+
+fastapi_app = FastAPI()
+fastapi_app.include_router(main_router)
+fastapi_app.mount("/static", StaticFiles(directory="static/"), name="static")
