44a46c0ea8b115b62ac22a7545563b180cd4824f
Vitaliy <vitaliy.srt1985@gmail.com>
2025-03-09 22:19:01 +0200

    modify service

--- src/utilitiesaccounting_v4/services.py
+++ src/utilitiesaccounting_v4/services.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+from decimal import Decimal
 from typing import List, TypeVar
 
 from src.utilitiesaccounting_v4.repository import SqlRepository
-from src.utilitiesaccounting_v4.tariff_manager import TariffManager
+from src.utilitiesaccounting_v4.tariff_manager import TariffManager, ConsumptionVolumeTariffManager, \
+    SubscriptionTariffManager
 from src.utilitiesaccounting_v4.uow import UnitOfWork
 
 REPO = TypeVar('REPO', bound=SqlRepository)
@@ -10,17 +12,44 @@ T = TypeVar("T", bound=TariffManager)
 
 
 class BaseService[T](ABC):
-    tariff_managers: List = []
+    category_name: str
+    tariff_managers: List[TariffManager] = []
 
     def __init__(self):
         self.storage_manager: type[UnitOfWork] = UnitOfWork
 
-    @abstractmethod
     def calc(self):
-        ...
+        sum_: Decimal = Decimal('0')
+        for tm in self.tariff_managers:
+            tariffs = self._get_tariffs(category_name=self.category_name,
+                                        tariff_type=tm.tariff_type)
+            sum_ += tm.calculate(tariffs)
+        return sum_
 
     def add_tariff_manager(self, tm: T):
         self.tariff_managers.append(tm)
+
+    def _get_tariffs(self, category_name: str, tariff_type: int):
+        with self.storage_manager() as sm:
+            tariffs = sm.tariff.get_tariffs_by_category_tariff_type(category_name, tariff_type)
+            return tariffs
+
+
+class ElectricService[T](BaseService):
+    category_name = 'Електропостачання'
+
+    tariff_managers = [
+        ConsumptionVolumeTariffManager()
+    ]
+
+
+class WaterService[T](BaseService):
+    category_name = 'Водопостачання'
+
+    tariff_managers = [
+        ConsumptionVolumeTariffManager(),
+        SubscriptionTariffManager(),
+    ]
 
 
 class CategoryService(BaseService):
@@ -54,11 +83,6 @@ class CounterService(BaseService):
 
 
 class CounterReadingService(BaseService):
-    def calc(self):
-        ...
-
-
-class ElectricService[T](BaseService):
     def calc(self):
         ...
 
