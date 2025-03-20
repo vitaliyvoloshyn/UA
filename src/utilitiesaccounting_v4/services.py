6461e6196d9c98566f1ec02706df92e13a3f6d03
Vitaliy <vitaliy.srt1985@gmail.com>
2025-03-20 19:00:28 +0200

    изменения в services.py: добавлен список инстансов базового класса сервисов

--- src/utilitiesaccounting_v4/services.py
+++ src/utilitiesaccounting_v4/services.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 from decimal import Decimal
-from typing import List, TypeVar
+from typing import List, TypeVar, Self
 
 from src.utilitiesaccounting_v4.repository import SqlRepository
+from src.utilitiesaccounting_v4.schemas.debt_dto import DebtDTO, TypeTariff
 from src.utilitiesaccounting_v4.tariff_manager import TariffManager, ConsumptionVolumeTariffManager, \
     SubscriptionTariffManager
 from src.utilitiesaccounting_v4.uow import UnitOfWork
@@ -14,17 +15,35 @@ T = TypeVar("T", bound=TariffManager)
 class BaseService[T](ABC):
     category_name: str
     tariff_managers: List[TariffManager] = []
+    instances: List[Self] = []
+    storage_manager: type[UnitOfWork] = UnitOfWork
+    a: int = 0
 
     def __init__(self):
-        self.storage_manager: type[UnitOfWork] = UnitOfWork
+        self._add_instance(self)
+
+    def calc_all_services(self):
+        return [s.calc() for s in self.instances]
 
     def calc(self):
-        sum_: Decimal = Decimal('0')
+        dept = []
         for tm in self.tariff_managers:
             tariffs = self._get_tariffs(category_name=self.category_name,
                                         tariff_type=tm.tariff_type)
-            sum_ += tm.calculate(tariffs)
-        return sum_
+            dept.extend(tm.calculate(tariffs))
+        return DebtDTO(
+            category_name=self.category_name,
+            debt=dept,
+            total_debt=self._get_total_debt(dept)
+        )
+
+    @staticmethod
+    def _get_total_debt(type_tariffs: List[TypeTariff]) -> str:
+        total: Decimal = Decimal('0')
+        for type_tariff in type_tariffs:
+            total = type_tariff + total
+
+        return str(total)
 
     def add_tariff_manager(self, tm: T):
         self.tariff_managers.append(tm)
@@ -33,6 +52,10 @@ class BaseService[T](ABC):
         with self.storage_manager() as sm:
             tariffs = sm.tariff.get_tariffs_by_category_tariff_type(category_name, tariff_type)
             return tariffs
+
+    def _add_instance(self, instance_: Self):
+        if instance_.__class__.__name__ not in [o.__class__.__name__ for o in self.instances]:
+            self.instances.append(instance_)
 
 
 class ElectricService[T](BaseService):
