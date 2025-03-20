f9e505e3de8755d71085587fc7a7d2c1bd747b34
Vitaliy <vitaliy.srt1985@gmail.com>
2025-03-20 18:20:07 +0200

    change tariff_manager.py

--- src/utilitiesaccounting_v4/tariff_manager.py
+++ src/utilitiesaccounting_v4/tariff_manager.py
@@ -4,12 +4,15 @@ from decimal import Decimal
 from typing import List
 from dateutil.relativedelta import relativedelta
 
+from src.utilitiesaccounting_v4.schemas.debt_dto import TypeTariff, SubscriptionTariff, OnTimeTariff, ConsumptionTariff
+
 
 class TariffManager[T](ABC):
     tariff_type: int
+    debt_dto: TypeTariff
 
     @abstractmethod
-    def calculate(self, schema: List[T]) -> Decimal:
+    def calculate(self, schema: List[T]) -> TypeTariff:
         ...
 
 
@@ -18,27 +21,39 @@ class SubscriptionTariffManager[T](TariffManager):
     Вытащить тариф
     """
     tariff_type = 1
+    debt_dto = SubscriptionTariff
 
-    def calculate(self, schema: List[T]) -> Decimal:
+    def calculate(self, schema: List[T]) -> List[TypeTariff]:
+        res: List[TypeTariff] = []
         start_date: datetime.date = schema[0].from_date
         inc_date: date = start_date
         final_date = date(date.today().year, date.today().month, 1) - relativedelta(days=1)
         sum_: Decimal = Decimal('0')
+        res_tariffs = {}
         while inc_date <= final_date:
             for tariff in schema:
                 if not tariff.to_date:
                     tariff.to_date = date(datetime.today().year, datetime.today().month, datetime.today().day)
                 if tariff.from_date <= inc_date < tariff.to_date:
                     sum_ += Decimal(str(tariff.value))
+                res_tariffs[tariff.name] = res_tariffs.setdefault(tariff.name, 0) + sum_
             inc_date += relativedelta(months=1)
-        print("sum_: ", sum_)
-        return sum_
+        for key, value in res_tariffs.items():
+            res.append(self.debt_dto(
+                value=str(value),
+                tariff_type_name=key
+            ))
+        return res
 
 
 class ConsumptionVolumeTariffManager[T](TariffManager):
     tariff_type = 2
-    def calculate(self, tariffs: List[T]):
+    debt_dto = ConsumptionTariff
+
+    def calculate(self, tariffs: List[T]) -> TypeTariff:
         sum_: Decimal = Decimal('0')
+        res: List[TypeTariff] = []
+        res_tariffs = {}
 
         for tariff in tariffs:
             if len(tariff.counter.counter_readings) > 1:
@@ -50,10 +65,26 @@ class ConsumptionVolumeTariffManager[T](TariffManager):
                         if tariff.from_date <= check_date <= tariff.to_date:
                             diff = cr.value - tariff.counter.counter_readings[index - 1].value
                             sum_ += Decimal(str(diff)) * Decimal(str(tariff.value))
-        return sum_
+            res_tariffs[tariff.name] = res_tariffs.setdefault(tariff.name, 0) + sum_
+        for key, value in res_tariffs.items():
+            res.append(self.debt_dto(
+                value=str(value),
+                tariff_type_name=key
+            ))
+        return res
 
 
 class OnTimeChargeTariffManager[T](TariffManager):
+    debt_dto = OnTimeTariff
+
     def calculate(self, tariffs: List[T]):
         sum_: Decimal = Decimal('0')
-        return sum((Decimal(str(tariff.value)) for tariff in tariffs))
+        res: List[TypeTariff] = []
+
+        for tariff in tariffs:
+            sum_ += Decimal(str(tariff.value))
+            res.append(self.debt_dto(
+                value=str(sum_),
+                tariff_type_name=tariff.name
+            ))
+        return res
