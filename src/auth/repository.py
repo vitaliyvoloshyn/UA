c2aaa9bd2838c1194e441ed44b86cf3dfcf43c36
Vitaliy <vitaliy.srt1985@gmail.com>
2025-09-04 18:57:46 +0300

    при добавлении нового пользователя в БД пароль хешируется

--- src/auth/repository.py
+++ src/auth/repository.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 from src.auth.models import User
 from src.auth.schemas import UserDTO, UserAddDTO
+from src.auth.service import user_exist, hash_password
 from src.core.repository.sqlalchemy_repository import SqlRepository
 from src.core.uow import StorageManager
 
@@ -13,11 +14,15 @@ class UserRepository(SqlRepository):
 
     def add(self, record: BaseModel):
         """Добавление нового пользователя. Но сначала проверяется отсутствие этого пользователя в системе"""
+        if user_exist(record.email):
+            record.password = hash_password(record.password)
+            super().add(record)
+
+    @staticmethod
+    def get_by_email(email: str):
         with StorageManager() as sm:
-            user = sm.userrepository.get(email=record.email)
-            if user:
-                raise ValueError('Користувач з таким email вже зареєстрований в системі')
-        super().add(record)
+            user = sm.userrepository.get(email=email)
+        return user or None
 
 
 StorageManager.register_repository(UserRepository)
